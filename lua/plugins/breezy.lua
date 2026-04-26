2aff361c07d5c97a8f555d8d0e8a72793827f75b
crrdvd <davide.corradini92@gmail.com>
2026-04-26 12:46:33 +0200

    Updated colorscheme

--- lua/plugins/breezy.lua
+++ lua/plugins/breezy.lua
@@ -1,10 +1,11 @@
-return {
-    "folke/tokyonight.nvim",
-    enabled = false,
+ return {
+    "fneu/breezy",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
     -- load the colorscheme here
-    vim.cmd([[colorscheme tokyonight]])
+    vim.cmd([[set background=light]])
+    vim.cmd([[set termguicolors]]) -- if you want to run vim in a terminal
+    vim.cmd([[colorscheme breezy]])
     end,
 }
