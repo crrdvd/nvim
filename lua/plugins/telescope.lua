9ccd9f9c54314aa5ff2c7ecf258c8ee4a31cf4ef
Davide Corradini <d.corradini@higecomore.com>
2026-05-06 14:23:24 +0200

    Aggiunto shortcut dd su telescope per eliminazione file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,5 +4,33 @@ return {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+    },
+    config = function()
+        local telescope = require('telescope')
+        local actions = require('telescope.actions')
+        local action_state = require('telescope.actions.state')
+
+        local delete_file = function(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            if not selection then return end
+            local confirm = vim.fn.input("Delete " .. selection.path .. "? [y/n]: ")
+            if confirm == "y" then
+                actions.close(prompt_bufnr)
+                vim.fn.delete(selection.path)
+                print("Deleted: " .. selection.path)
+            end
+        end
+
+        telescope.setup({
+            defaults = {
+                mappings = {
+                    n = {
+                        ["dd"] = delete_file,
+                    },
+                },
+            },
+        })
+
+        telescope.load_extension('fzf')
+    end,
 }
