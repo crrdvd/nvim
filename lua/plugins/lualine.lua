163dafc26857b48bf9c02d103b08cd3bf25eca10
Davide Corradini <d.corradini@higecomore.com>
2026-05-12 22:43:27 +0200

    Updated lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,15 +4,16 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "nightfox",
+        theme = "auto",
+        globalstatus = true,
         component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
       },
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { { "filename", path = 1 } },
-        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_x = { "encoding", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
