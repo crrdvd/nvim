1e74087829494e1b3f19b36d9a7e3606de93dee8
Davide Corradini <d.corradini@higecomore.com>
2026-05-26 08:58:06 +0200

    Added inline diagnostics

--- lua/settings.lua
+++ lua/settings.lua
@@ -76,6 +76,13 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
+vim.diagnostic.config({
+  virtual_text = true,  -- Enable inline virtual text
+  signs = true,         -- Show icons in the sign column
+  underline = true,
+  update_in_insert = false,
+})
+
 ---------------
 ----KEYMAPS----
 ---------------
