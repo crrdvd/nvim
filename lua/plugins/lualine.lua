f437223011a8066fc7579402d8693f6067bcb989
Davide Corradini <d.corradini@higecomore.com>
2026-07-13 14:53:36 +0200

    Git blame plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,6 @@
+vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+local git_blame = require('gitblame')
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -12,8 +15,19 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { { "filename", path = 1 } },
-        lualine_x = { "encoding", "filetype" },
+        lualine_c = {
+          {
+            "filename",
+            path = 1
+          }
+        },
+        lualine_x = {
+          {
+            git_blame.get_current_blame_text,
+            cond = git_blame.is_blame_text_available
+          },
+          "filetype"
+        },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
